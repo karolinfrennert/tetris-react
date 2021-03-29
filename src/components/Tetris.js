0d2c6fb946280ef3d89c7bbe0436bc58d9f82aca
karolinfrennert <karolinfrennert@gmail.com>
2021-03-29 21:20:38 +0200

    added custom hooks

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,12 +1,24 @@
+import { useState } from "react";
 import { createStage } from "../gameHelpers";
 
 //components
 import Display from "./Display";
 import Stage from "./Stage";
 import StartButton from "./StartButton";
+//styled components
 import { StyledTetris, StyledTetrisWrapper } from "./styles/StyledTetris";
 
+//custom hooks
+import { usePlayer } from "../hooks/usePlayer";
+import { useStage } from "../hooks/useStage";
+
 const Tetris = () => {
+  const [dropTime, setDropTime] = useState(null);
+  const [gameOver, setGameOver] = useState(false);
+  const [player] = usePlayer();
+  const [stage, setStage] = useStage(player);
+
+  console.log("Rerendered");
   return (
     <StyledTetrisWrapper>
       <StyledTetris>
