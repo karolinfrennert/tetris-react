b9c7d4474673ef58973923a849f79b1724fbeb96
karolinfrennert <karolinfrennert@gmail.com>
2021-04-01 21:59:54 +0200

    tetrominos

--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -5,7 +5,7 @@ import { randomTetromino, TETROMINOS } from "../tetrominos";
 export const usePlayer = () => {
   const [player, setPlayer] = useState({
     pos: { x: 0, y: 0 },
-    tetromino: randomTetromino().shape,
+    tetromino: TETROMINOS[0].shape,
     collided: false,
   });
 
--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -8,9 +8,23 @@ export const useStage = (player, resetPlayer) => {
       const newStage = prevStage.map((row) =>
         row.map((cell) => (cell[1] === "clear" ? [0, "clear"] : cell))
       );
+
+      player.tetromino.forEach((row, y) => {
+        row.forEach((value, x) => {
+          if (value !== 0) {
+            newStage[y + player.pos.y][x + player.pos.x] = [
+              value,
+              `${player.collided ? "merged" : "clear"}`,
+            ];
+          }
+        });
+      });
+
+      return newStage;
     };
+
     setStage((prev) => updateStage(prev));
-  }, []);
+  }, [player.collided, player.pos.x, player.pos.y, player.tetromino]);
 
   return [stage, setStage];
 };
