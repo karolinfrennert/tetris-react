546de85c2404bc8d1270f6da09a19f5e23aa3b5e
karolinfrennert <karolinfrennert@gmail.com>
2021-04-07 16:14:43 +0200

    If I learn how to spell length I wont have these type of bugs oh man

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -4,14 +4,14 @@ import { createStage } from "../gameHelpers";
 export const useStage = (player, resetPlayer) => {
   const [stage, setStage] = useState(createStage());
   const [rowsCleared, setRowsCleared] = useState(0);
+
   useEffect(() => {
     setRowsCleared(0);
-
     const sweepRows = (newStage) =>
       newStage.reduce((ack, row) => {
         if (row.findIndex((cell) => cell[0] === 0) === -1) {
           setRowsCleared((prev) => prev + 1);
-          ack.unshift(new Array(newStage[0].lenght).fill([0, "clear"]));
+          ack.unshift(new Array(newStage[0].length).fill([0, "clear"]));
           return ack;
         }
         ack.push(row);
--- src/tetrominos.js
+++ src/tetrominos.js
@@ -1,5 +1,5 @@
 export const TETROMINOS = {
-  0: { shape: [[0]], color: "0,0,0" },
+  0: { shape: [[0]], color: "0, 0, 0" },
   I: {
     shape: [
       [0, "I", 0, 0],
@@ -11,7 +11,6 @@ export const TETROMINOS = {
   },
   J: {
     shape: [
-      [0, "J", 0],
       [0, "J", 0],
       [0, "J", 0],
       ["J", "J", 0],
@@ -31,7 +30,7 @@ export const TETROMINOS = {
       ["O", "O"],
       ["O", "O"],
     ],
-    color: "223, 173, 36",
+    color: "223, 217, 36",
   },
   S: {
     shape: [
@@ -43,9 +42,9 @@ export const TETROMINOS = {
   },
   T: {
     shape: [
+      [0, 0, 0],
       ["T", "T", "T"],
       [0, "T", 0],
-      [0, 0, 0],
     ],
     color: "132, 61, 198",
   },
