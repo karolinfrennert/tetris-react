15a979a81bc8d695688c87bcb777bda624d9f1d1
karolinfrennert <karolinfrennert@gmail.com>
2021-04-07 16:57:55 +0200

    img width correct

--- src/components/styles/StyledTetris.js
+++ src/components/styles/StyledTetris.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import bgImage from "../../images/space.jpeg";
 
 export const StyledTetrisWrapper = styled.div`
-  width: 100vh;
+  width: 100vw;
   height: 100vh;
   background: url(${bgImage}) #000;
   background-size: cover;
